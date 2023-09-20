a3f52167609e70214bc8d96faf99c99bccc12d30
Artandrey <artandrey7777@gmail.com>
2023-09-20 18:27:38 +0300

    its bouncing

---
+++ Assets/Scripts/Player/Jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float thrust = 8.0f;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+
+    }
+
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        string name = this.name;
+
+        if (name == "collider-top")
+        {
+            rb.AddForce(-transform.up * thrust, ForceMode2D.Impulse);
+
+        }
+        else if (name == "collider-bottom")
+        {
+            rb.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+        }
+    }
+}
