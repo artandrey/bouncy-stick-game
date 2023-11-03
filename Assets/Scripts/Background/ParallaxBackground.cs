b4367d55fb611ac72a9468c95cac138ffd2c13e3
Andrey Ruban <anruban2001@gmail.com>
2023-11-03 12:32:17 +0200

    fix(no)

--- Assets/Scripts/Background/ParallaxBackground.cs
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ParallaxBackgroud: MonoBehaviour
+public class ParallaxBackground : MonoBehaviour
 {
     [SerializeField] private Vector2 parallaxEffectMultiplier;
 
@@ -11,7 +11,7 @@ public class ParallaxBackgroud: MonoBehaviour
     private void Start()
     {
         cameraTransform = Camera.main.transform;
-        lastCameraPosition= cameraTransform.position;
+        lastCameraPosition = cameraTransform.position;
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
         Texture2D texture = sprite.texture;
         textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
@@ -23,7 +23,7 @@ public class ParallaxBackgroud: MonoBehaviour
         transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
         lastCameraPosition = cameraTransform.position;
 
-        if (Mathf.Abs(cameraTransform.position.x - transform.position.x)>= textureUnitSizeX)
+        if (Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
         {
             float offsetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
             transform.position = new Vector3(cameraTransform.position.x + offsetPositionX, transform.position.y);
