4697073ac6a68984ef6f3621fe14069a6d7794b1
Artandrey <artandrey7777@gmail.com>
2024-01-02 21:26:42 +0200

    implemented level loader

--- Assets/LoadScreenControl.cs
+++ Assets/LoadScreenControl.cs
@@ -1,38 +1,25 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadScreenControl : MonoBehaviour
 {
     [SerializeField]
     private Animator animator;
+
     public event Action onScreenOffEnded;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void ScreenOn()
     {
-        animator.SetInteger("State", 1);
+        animator.SetTrigger("ScreenOn");
+    }
+
+    public void ScreenOff()
+    {
+        animator.SetTrigger("ScreenOff");
     }
 
     public void ScreenOffCompleted()
     {
-        Debug.Log("ScreenOffCompleted");
         onScreenOffEnded?.Invoke();
-        animator.SetInteger("State", 0);
-
     }
-
-
 }
