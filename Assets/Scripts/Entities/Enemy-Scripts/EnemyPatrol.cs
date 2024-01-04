87944db27fdf9ac32b0dda987ab5d18c6c6a26fe
Artandrey <artandrey7777@gmail.com>
2024-01-04 21:50:41 +0200

    resolved conflicts, fixed animations, refactored code

--- Assets/Scripts/Entities/Enemy-Scripts/EnemyPatrol.cs
+++ Assets/Scripts/Entities/Enemy-Scripts/EnemyPatrol.cs
@@ -1,26 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class enemyPatrol : MonoBehaviour
+public class EnemyPatrol : MonoBehaviour
 {
-    public GameObject pointA;
-    public GameObject pointB;
-    public float speed;
+    [SerializeField]
+    private GameObject pointA;
+    [SerializeField]
+    private GameObject pointB;
+    [SerializeField]
+    private float speed;
     private Rigidbody2D rb;
-    private Animator anim;
     private Transform currentPoint;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
         currentPoint = pointB.transform;
-        anim.SetBool("isRunning", true);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 point = currentPoint.position - transform.position;
