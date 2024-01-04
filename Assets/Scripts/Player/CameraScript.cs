3c6f5633c9954fa252a181a7f29c95078fea9214
Karlson54 <anruban2001@gmail.com>
2024-01-04 21:03:19 +0200

    adding parallax

---
+++ Assets/Scripts/Player/CameraScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = player.transform.position.x;
+        float y = player.transform.position.y;
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+
+}
