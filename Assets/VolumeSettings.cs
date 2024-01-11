6c7a8a21399edca58cc796c8a545e16703a84444
Artandrey <artandrey7777@gmail.com>
2024-01-11 21:34:20 +0200

    added menu disable visible button state

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -11,6 +11,14 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXSlider;
     [SerializeField] private Toggle myToggle;
+    [SerializeField]
+    private Image musicButtonImage;
+    [SerializeField]
+
+    private Image sfxButtonImage;
+
+    private readonly Color red = new Color(243f / 255f, 174f / 255f, 174f / 255f);
+    private readonly Color white = Color.white;
     private bool audioIn;
     private float MusicVolume;
     private float SFXVolume;
@@ -31,20 +39,22 @@ public class VolumeSettings : MonoBehaviour
 
     public void SetMusicVolume(float volume)
     {
+        MusicVolume = volume;
         musicSlider.value = volume;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        musicButtonImage.color = MusicVolume == MinVolume ? red : white;
         PlayerPrefs.SetFloat("musicVolume", volume);
-        MusicVolume = volume;
         Debug.Log(volume);
     }
-    
+
 
     public void SetSFXVolume(float volume)
     {
+        SFXVolume = volume;
         SFXSlider.value = volume;
+        sfxButtonImage.color = SFXVolume == MinVolume ? red : white;
         myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
-        SFXVolume = volume;
         Debug.Log(volume);
     }
 
