01dc20a95916fb7cd9dce07f781f384143ffa5e3
Artandrey <artandrey7777@gmail.com>
2024-01-11 13:51:19 +0200

    fixed level position

--- Assets/Scripts/Entities/Enemy-Scripts/Squirrel/Squirrel.cs
+++ Assets/Scripts/Entities/Enemy-Scripts/Squirrel/Squirrel.cs
@@ -4,30 +4,54 @@ using UnityEngine;
 
 public class Squirrel : MonoBehaviour
 {
+
     private float timer = 0;
 
     [SerializeField]
     private float throwDelay;
 
     [SerializeField]
-    private float throwStrength;
+    private float throwForce;
     [SerializeField]
     private GameObject throwedObject;
     [SerializeField]
     private Transform throwPoint;
 
+    internal SquirrelIdleState idleState = new();
+    internal SquirrelThrowingState throwingState = new();
+
+    private Animator squirrelAnimator;
+
+    void Start()
+    {
+        squirrelAnimator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         if (timer < 0)
         {
             timer = throwDelay;
-            OnTrow();
+            StartThrow();
         }
     }
 
-    protected void OnTrow()
+    private void StartThrow()
     {
 
+    }
+
+    private void OnTrow()
+    {
+        GameObject instance = Instantiate(throwedObject);
+
+        instance.transform.position = throwPoint.position;
+        instance.transform.rotation = throwPoint.rotation;
+
+        Rigidbody2D rigidbody = instance.GetComponent<Rigidbody2D>();
+
+        Vector2 throwDirection = new Vector2(Mathf.Cos(throwPoint.eulerAngles.z * Mathf.Deg2Rad), Mathf.Sin(throwPoint.eulerAngles.z * Mathf.Deg2Rad));
+
+        rigidbody.AddForce(throwDirection * throwForce, ForceMode2D.Impulse);
     }
 }
