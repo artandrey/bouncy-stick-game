7a2d77379c06b489eba1ee69d6321e137b651075
Karlson54 <anruban2001@gmail.com>
2024-01-11 19:10:05 +0200

    adding mute button

--- Assets/VolumeSettings.cs
+++ Assets/VolumeSettings.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Xml.Serialization;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
@@ -8,6 +10,11 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Toggle myToggle;
+    private bool audioIn;
+    private float MusicVolume;
+    private float SFXVolume;
+    readonly float MinVolume = 0.0001f;
 
     private void Start()
     {
@@ -17,31 +24,67 @@ public class VolumeSettings : MonoBehaviour
         }
         else
         {
-            SetMusicVolume();
-            SetSFXVolume();
+            SetMusicVolume(0);
+            SetSFXVolume(0);
         }
     }
 
-    public void SetMusicVolume()
+    public void SetMusicVolume(float volume)
     {
-        float volume = musicSlider.value;
+        musicSlider.value = volume;
         myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("musicVolume", volume);
+        MusicVolume = volume;
+        Debug.Log(volume);
     }
+    
 
-    public void SetSFXVolume()
+    public void SetSFXVolume(float volume)
     {
-        float volume = SFXSlider.value;
+        SFXSlider.value = volume;
         myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
         PlayerPrefs.SetFloat("SFXVolume", volume);
+        SFXVolume = volume;
+        Debug.Log(volume);
+    }
+
+    public void ToggleSFX()
+    {
+        if (SFXVolume == MinVolume)
+        {
+            SetSFXVolume(1);
+        }
+        else
+        {
+            SetSFXVolume(MinVolume);
+        }
+    }
+
+    public void ToggleMusic()
+    {
+        if (MusicVolume == MinVolume)
+        {
+            SetMusicVolume(1);
+        }
+        else
+        {
+            SetMusicVolume(MinVolume);
+        }
+    }
+
+    public void OnMusicSliderChange()
+    {
+        SetMusicVolume(musicSlider.value);
+    }
+
+    public void OnSFXSliderChange()
+    {
+        SetSFXVolume(SFXSlider.value);
     }
 
     private void LoadVolume()
     {
-        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
-        SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
-
-        SetMusicVolume();
-        SetSFXVolume();
+        SetMusicVolume(PlayerPrefs.GetFloat("musicVolume"));
+        SetSFXVolume(PlayerPrefs.GetFloat("SFXVolume"));
     }
 }
